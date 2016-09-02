22f6f3c602b11f9f99dca02fbf273b047b4f3789
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2016-09-02 14:49:28 +1200

    Added a 'change' option for creating native events.

--- js/utils/window.create_native_event.js
+++ js/utils/window.create_native_event.js
@@ -4,15 +4,29 @@
 		var e;
 		if (d.createEvent) {
 			e = d.createEvent("MouseEvent");
-			e.initMouseEvent(eventType,true,true,w,0,0,0,0,0,false,false,false,false,0,null)
+			e.initMouseEvent(eventType,true,true,w,0,0,0,0,0,false,false,false,false,0,null);
 		} else {
 			e = new MouseEvent(eventType)
 		}
 		return e;
 	};
 
+	var _createChangeEvent = function(eventType){
+		var e;
+		if (d.createEvent) {
+			e = d.createEvent("HTMLEvents");
+			e.initEvent(eventType,true,true);
+		} else {
+			e = new Event(eventType, {bubbles: true});
+		}
+		return e;
+	};
+
 	utils.createNativeEvent = function(eventType){
 		switch (eventType) {
+			case 'change':
+				return _createChangeEvent(eventType);
+				break;
 			case 'mousedown':
 			case 'mouseup':
 			case 'click':
