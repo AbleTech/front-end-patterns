53b0967d9eb2bd2ab4f2179cf89d1328e48bbf9d
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2016-09-05 11:57:47 +1200

    Update createNativeEvent and spec.

--- test/test.window.create_native_event.js
+++ test/test.window.create_native_event.js
@@ -1,15 +1,30 @@
-describe('utils.window.create_native_event', function() {
+describe('utils.createNativeEvent', function() {
 
-	if (document.createEvent) {
-		it('should create a `initMouseEvent` in a non-compliant browser', function() {
-			var clickEvent = utils.createNativeEvent('click');
-			assert.equal(typeof clickEvent, 'object');
-		});
-	} else {
-		it('should create a `MouseEvent` in a compliant browser', function() {
-			var clickEvent = utils.createNativeEvent('click');
-			assert.equal(typeof clickEvent, 'object');
-		});
-	}
+	var clickEvent,
+	    mouseDownEvent,
+	    mouseUpEvent,
+	    changeEvent;
+
+	it('should create a click event', function() {
+		clickEvent = utils.createNativeEvent('click');
+		assert.equal(clickEvent.type, 'click');
+	});
+
+	it('should create a mousedown event', function() {
+		mouseDownEvent = utils.createNativeEvent('mousedown');
+		assert.equal(mouseDownEvent.type, 'mousedown');
+	});
+
+	it('should create a mouseup event', function() {
+		mouseUpEvent = utils.createNativeEvent('mouseup');
+		assert.equal(mouseUpEvent.type, 'mouseup');
+	});
+
+	it('should create a change event', function() {
+		changeEvent = utils.createNativeEvent('change');
+		assert.equal(changeEvent.type, 'change');
+	});
+
+
 
 });
