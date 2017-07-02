aa49109f7ca1c755c6d637c4b4ff5004d4fcf963
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2017-07-02 20:51:56 +1200

    Add UI.AddClass.js

---
+++ js/UI/ui.add_class.js
@@ -0,0 +1,29 @@
+// Requires ./_ui
+(function(d){
+
+  var ATTR_ADD_CLASS = 'data-add-class';
+
+  function AddClass(){
+    var ac = this;
+    ac.elems = null; // NodeList
+
+    /* Private ******************************/
+    function _ManipulateElem(elem) {
+      elem.classList.add(elem.getAttribute(ATTR_ADD_CLASS));
+      elem.removeAttribute(ATTR_ADD_CLASS);
+    }
+    /****************************************/
+
+    ac.init = function(){
+      ac.elems = d.querySelectorAll('[' + ATTR_ADD_CLASS + ']');
+      if (ac.elems.length) {
+        ac.elems.forEach( _ManipulateElem );
+      }
+    };
+
+    return ac;
+  }
+
+  UI.AddClass = AddClass;
+
+})(document);
